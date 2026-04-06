a668197e6fce8d2098a54e1dda5e50807bf7e7f3
PMenchen <72562238+PMenchen@users.noreply.github.com>
2026-04-06 20:34:04 +0200

    Archivos despliegue web en render

--- laravel-backend/config/cors.php
+++ laravel-backend/config/cors.php
@@ -15,7 +15,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200', 'http://127.0.0.1:4200'],
+    'allowed_origins' => [
+        'https://liga-deportiva-frontend.onrender.com',
+        'http://localhost:4200'
+    ],
 
     'allowed_origins_patterns' => [],
 
---
+++ liga-deportiva/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://liga-deportiva-api.onrender.com/api'
+};
